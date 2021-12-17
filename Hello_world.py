3dd6112415de2e4bd6642ef92b6476026959d3cb
namanikanta1116 <manikanta.nadella1116@gmail.com>
2021-12-17 11:24:39 +0530

    python

---
+++ Hello_world.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+import sys
+
+if __name__ == '__main__':
+     print("this script was run at " + str(datetime.now()))
+
+x=45
+y=55
+z=x+y
+print("Sum of two given numbers:-",z)
